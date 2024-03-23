a83d42c281254c401bbe22b4049021fc83a93682
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-03-23 20:46:58 +0800

    Testing: Print name, age, dream job

--- fancy_text.py
+++ fancy_text.py
@@ -10,6 +10,12 @@ age = input("Enter your age: ")
 dream_job = input("What is your dream job?: ")
 
 # print user's name in fancy way
+print(name)
+
 # print the age in fancy age
+print(age)
+
 # print dream job in a fancy way
+print(dream_job)
+
 # provide a short message
