de8a6bac77198134d9161a1a6d37cda7709aa1bd
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-03-23 20:31:35 +0800

    Added a pseudocode (1)

---
+++ fancy_text.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# ask user to input name
+# ask user to input age
+# ask user to input their dream job
+# print user's name in fancy way
+# print the age in fancy age
+# print dream job in a fancy way
+# provide a short message
