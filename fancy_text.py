d3287f6734c561468228ab164e9b78d95f57599e
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-03-23 21:00:12 +0800

    Add horizontal border

--- fancy_text.py
+++ fancy_text.py
@@ -2,6 +2,7 @@ from pyfiglet import figlet_format
 
 # pseudocode
 
+
 # ask user to input name
 name = input("Enter your name: ")
 
@@ -11,6 +12,9 @@ age = input("Enter your age: ")
 # ask user to input their dream job
 dream_job = input("What is your dream job?: ")
 
+
+# add horizontal border 
+print("=" * 100)
 # print user's name in fancy way
 print(figlet_format(name, font="univers"))
 
@@ -21,3 +25,6 @@ print(figlet_format(age, font="univers"))
 print(figlet_format(dream_job, font="univers"))
 
 # provide a short message
+
+# add horizontal border 
+print("=" * 100)
